a5900c20af33c927089e27ae49bf6ba74ccd0e39
Ayildiz89 <yildizali87@hotmail.com>
2020-07-01 04:57:04 +0200

    server.js guncellendi

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ response.writeHeader(200,{'Context-Type':'text-plain'});
 response.write('<html><body><h1>Kod Denemeleri</h1></body></html>');
 response.end();
 }
-http.createServer(benimFonksiyonum).listen(49160);
+http.createServer(benimFonksiyonum).listen(8005);
 console.log('Sunucu Calisiyor');
 
